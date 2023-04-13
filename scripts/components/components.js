b0cb1c82c01cb77a8b852000a107a086d7bc3b27
Nurul Azizah <idlionora@gmail.com>
2023-04-13 14:48:05 +0700

    feat(detail): show trailer and add stream episodes section

--- scripts/components/components.js
+++ scripts/components/components.js
@@ -1,17 +1,9 @@
-export const bannerCarouselItem = ({
-  id,
-  title,
-  description,
-  banner,
-  year,
-  genre,
-  format,
-}) => {
-  if (description.length > 150) {
-    description = description.substring(0, 150) + '...';
-  }
+export const bannerCarouselItem = ({ id, title, description, banner, year, genre, format }) => {
+	if (description.length > 150) {
+		description = description.substring(0, 150) + '...';
+	}
 
-  return `
+	return `
     <a href="/detail.html?id=${id}" class="hidden duration-200 ease-linear flex" data-carousel-item>
       <div class="basis-2/5 px-16 py-8">
         <h1 class="mb-2 text-4xl font-bold leading-normal tracking-tight text-white">${title}</h1>
@@ -32,7 +24,7 @@ export const bannerCarouselItem = ({
 };
 
 export const section = ({ name, data }) => {
-  return `
+	return `
     <section id="${name.toLowerCase()}" class="mt-16 relative">
       <div class="px-16">
         <h4 class="text-2xl font-bold text-white">${name.replace(/-/g, ' ')}</h4>
@@ -40,16 +32,16 @@ export const section = ({ name, data }) => {
           <div id="${name.toLowerCase()}-container"
             class="flex flex-nowrap overflow-x-scroll scroll-smooth hide-scroll-bar gap-x-4 snap-x snap-mandatory">
             ${data
-              .map((item) =>
-                imageCard({
-                  id: item.id,
-                  title: item.title.romaji,
-                  imageUrl: item.coverImage,
-                  year: item.year,
-                  genre: item.genres[0],
-                })
-              )
-              .join('')}
+				.map((item) =>
+					imageCard({
+						id: item.id,
+						title: item.title.romaji,
+						imageUrl: item.coverImage,
+						year: item.year,
+						genre: item.genres[0],
+					})
+				)
+				.join('')}
           </div>
         </div>
       </div>
@@ -81,20 +73,96 @@ export const section = ({ name, data }) => {
 };
 
 export const imageCard = ({ id, title, imageUrl, year, genre }) => {
-  if (title.length > 50) {
-    title = title.substring(0, 50) + '...';
-  }
-
-  return `
+	if (title.length > 50) {
+		title = title.substring(0, 50) + '...';
+	}
+	return `
     <a href="detail.html?id=${id}" class="snap-start cursor-pointer w-56">
       <div class="w-56 h-80 overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
         <img class="block object-cover h-80 w-80" src="${imageUrl}" alt="${title}">
       </div>
       <div class="mt-4">
         <p class="text-white">${title}</p>
-        <p class="text-sm text-gray-400 mt-1">${
-          year ? year + ' • ' : ''
-        }${genre}</p>
+        <p class="text-sm text-gray-400 mt-1">${year ? year + ' • ' : ''}${genre}</p>
+      </div>
+    </a>
+  `;
+};
+
+
+const sliderControl = () => {
+	return `
+      <button type="button"
+        class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        data-carousel-prev>
+        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10">
+          <svg aria-hidden="true" class="w-10 h-10 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+          </svg>
+          <span class="sr-only">Previous</span>
+        </span>
+      </button>
+      <button type="button"
+        class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        data-carousel-next>
+        <span class="inline-flex items-center justify-center w-8 h-8 sm:w-10 sm:h-10">
+          <svg aria-hidden="true" class="w-10 h-10 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+          </svg>
+          <span class="sr-only">Next</span>
+        </span>
+      </button>
+  `;
+};
+
+export const detailCarousel = (streamingEpisodes) => {
+	return `
+      <h2 class="w-full max-w-[74rem] text-lg ">Stream Episodes</h2>
+        <div class="relative mt-3 w-full max-w-[74rem] flex">
+          <button id="ep-carousel-prev" class="flex flex-col justify-center cursor-pointer shrink-0 grow-0">
+            <svg aria-hidden="true" class="w-10 h-10 pr-3.5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+            </svg>
+            <span class="sr-only">Previous</span>
+          </button>
+          <div id="episodes-container" class="flex flex-nowrap overflow-x-scroll scroll-smooth hide-scroll-bar gap-x-5 snap-x snap-mandatory">
+            ${streamingEpisodes
+				.map((episode) =>
+					episodeCard({
+						title: episode.title,
+						url: episode.url,
+						thumbnail: episode.thumbnail,
+						site: episode.site,
+					})
+				)
+				.join('')}
+          </div>
+          <button id="ep-carousel-next" class="flex flex-col justify-center cursor-pointer shrink-0 grow-0">
+            <svg aria-hidden="true" class="w-10 h-10 pl-3.5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+          </svg>
+            <span class="sr-only">Previous</span>
+          </button>
+        </div>
+  `;
+};
+
+const episodeCard = ({ title, url, thumbnail, site }) => {
+	if (title.length > 50) {
+		title = title.substring(0, 50) + '...';
+	}
+	return `
+    <a href="${url}" class="snap-start cursor-pointer w-56">
+      <div class="w-[10.3rem] h-56 overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+        <img class="block object-cover h-56 w-56" src="${thumbnail}" alt="${title}">
+      </div>
+      <div class="mt-4">
+        <p class="text-white">${title}</p>
+        <p class="text-sm text-gray-400 mt-1">${site}</p>
       </div>
     </a>
   `;
--- scripts/detail.js
+++ scripts/detail.js
@@ -1,3 +1,4 @@
+import { detailCarousel } from './components/components.js';
 import { loadingDetail } from './data.js';
 import { initialSetup } from './index.js';
 
@@ -8,69 +9,96 @@ const descriptionEl = document.getElementById('description');
 const bannerImageEl = document.getElementById('banner-image');
 const coverImageEl = document.getElementById('cover-image');
 const infoEl = document.getElementById('info');
-const infoListEl = document.getElementById('info-list')
+const infoListEl = document.getElementById('info-list');
+const trailerEl = document.getElementById('trailer-div');
+const streamEpisodeEl = document.getElementById('stream-episode');
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
 
-const getDetailData = async() => {
-  if (id === '999999') {
-    titleEl.innerHTML = 'NOT FOUND';
-    descriptionEl.innerHTML = 'Data is not registered.';
-    return;
-  }
+const getDetailData = async () => {
+	if (id === '999999') {
+		titleEl.innerHTML = 'NOT FOUND';
+		descriptionEl.innerHTML = 'Data is not registered.';
+		return;
+	}
 
-  try {
-    const response = await axios.get(`https://animeapi-askiahnur1.b4a.run/anime/${id}`);
-    updateDetailPage(response.data)
-    console.log(response.data)
-  } catch (err) {
-    console.error('Failed to get data for detail page: ', err.message);
-    titleEl.innerHTML = 'NOT FOUND'
-    descriptionEl.innerHTML = `Failed to get requested data: ${err.message}`;
-  }
-}
+	try {
+		const response = await axios.get(`https://animeapi-askiahnur1.b4a.run/anime/${id}`);
+		updateDetailPage(response.data);
+		console.log(response.data);
+	} catch (err) {
+		console.error('Failed to get data for detail page: ', err.message);
+		titleEl.innerHTML = 'NOT FOUND';
+		descriptionEl.innerHTML = `Failed to get requested data: ${err.message}`;
+	}
+};
 
 const updateDetailPage = (data) => {
-  titleEl.innerHTML = data.title.romaji;
-  descriptionEl.innerHTML = data.description;
-  bannerImageEl.src = data.bannerImage || loadingDetail.bannerImage;
-  coverImageEl.src = data.coverImage || loadingDetail.coverImage;
+	titleEl.innerHTML = data.title.romaji;
+	descriptionEl.innerHTML = data.description;
+	bannerImageEl.src = data.bannerImage || loadingDetail.bannerImage;
+	coverImageEl.src = data.coverImage || loadingDetail.coverImage;
+	if (!!data.trailer?.site && !!data.trailer?.id)
+		trailerEl.innerHTML = `<iframe id="trailer-vid" width="420" height="315" src="https://www.${data.trailer.site}.com/embed/${data.trailer.id}" frameborder="0">
+            </iframe>`;
 
-  const info = [data.year, data.format, data.episodes ? `${data.episodes}&nbsp;Episode${data.episodes > 1?'s':''}` : ''];
+	const info = [
+		data.year,
+		data.format,
+		data.episodes ? `${data.episodes}&nbsp;Episode${data.episodes > 1 ? 's' : ''}` : '',
+	];
 
-  let infoString = '';
-  info.forEach((item, index) => {
+	let infoString = '';
+	info.forEach((item, index) => {
 		if (item) {
 			infoString += `<span class="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">${item}</span>`;
 		}
-  });
-  infoEl.innerHTML = infoString;
+	});
+	infoEl.innerHTML = infoString;
 
-  const infoList = {
-		'Title': data.title.romaji,
-    "English Title": data.title.english,
-		'Format': data.format,
-		'Episode': data.episodes,
-		'Status': data.status,
-		'Genres': data.genres,
-		'Popularity': data.popularity,
-  };
+	const infoList = {
+		Title: data.title.romaji,
+		'English Title': data.title.english,
+		Format: data.format,
+		Episode: data.episodes,
+		Status: data.status,
+		Genres: data.genres,
+		Popularity: data.popularity,
+	};
 
-  const infoKeyArray = Object.keys(infoList);
-  const infoListString = infoKeyArray.map((infoKey, index) => { let infoItem = infoList[infoKey];
-    if (infoKey == 'Genres') {infoItem = infoList[infoKey].join(', ')}; 
-    return `<li id='infokey${index}'><b>${(infoKey == 'Episode' && infoList[infoKey] > 1)? infoKey + 's': infoKey}:</b> ${!!infoItem?infoItem:'?'}</li>`;
-  }).join(' ');
-  console.log(infoListString);
+	const infoKeyArray = Object.keys(infoList);
+	const infoListString = infoKeyArray
+		.map((infoKey, index) => {
+			let infoItem = infoList[infoKey];
+			if (infoKey == 'Genres') {
+				infoItem = infoList[infoKey].join(', ');
+			}
+			return `<li id='infokey${index}'><b>${
+				infoKey == 'Episode' && infoList[infoKey] > 1 ? infoKey + 's' : infoKey
+			}:</b> ${!!infoItem ? infoItem : '?'}</li>`;
+		})
+		.join(' ');
 
-  infoListEl.innerHTML = infoListString;
-}
-updateDetailPage(loadingDetail)
-getDetailData()
+	infoListEl.innerHTML = infoListString;
 
+	if (data.streamingEpisodes.length > 0) {
+    streamEpisodeEl.innerHTML = detailCarousel(data.streamingEpisodes)
 
+    const prev = document.getElementById('ep-carousel-prev');
+    prev.onclick = () => {
+		  document.getElementById('episodes-container').scrollLeft -= 1000;
+	  };
+
+    const next = document.getElementById('ep-carousel-next');
+	    next.onclick = () => {
+		document.getElementById('episodes-container').scrollLeft += 1000;
+	};
+  }
+};
+updateDetailPage(loadingDetail);
+getDetailData();
 
 //? tambahkan script untuk menampilkan daftar genre
 // .....
